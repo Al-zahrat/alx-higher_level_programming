2852547ae75c142fa6664c6c4f865f9ac92d161f
zahra <ezzahraelidrissi1@gmail.com>
2024-03-19 00:33:01 +0000

    data structure

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    x = []
+    if id < 0 or len(my_list) <= idx:
+        return my_list
+    else:
+        for i in range(0, len(my_list)):
+            if i == idx:
+                x.append(element)
+            else:
+                x.append(my_list[i])
+        return x
