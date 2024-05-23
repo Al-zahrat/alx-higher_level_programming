25dff5478971245486de540291ef951f621bfaaf
ezzahra <sindyzahra26@gmail.com>
2024-05-23 17:41:53 +0000

    sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,4 @@
 -- converts hbtn_0c_0 database to UTF8
 ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
-ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE fhbtn_0c_.0irst_table MODIFY name CHARACTER SET utf8mb4 COLLATE f8mb4_unicode_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table MODIFY name CHARACTER SET utf8mb4 COLLATE f8mb4_unicode_ci;
