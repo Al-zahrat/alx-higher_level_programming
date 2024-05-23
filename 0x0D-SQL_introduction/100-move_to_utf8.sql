b60db043e500bd30eb4239f247071b4d096381fc
ezzahra <sindyzahra26@gmail.com>
2024-05-23 23:42:49 +0000

    ex-sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,9 +1,9 @@
 -- alter hbtn_0c_0 database
 ALTER DATABASE `hbtn_0c_0`
-CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 USE `hbtn_0c_0`;
 ALTER TABLE `first_table`
-CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 ALTER TABLE `first_table`
 MODIFY `name` VARCHAR(256)
-CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CHARACTER SET utf8 COLLATE utf8_unicode_ci;
