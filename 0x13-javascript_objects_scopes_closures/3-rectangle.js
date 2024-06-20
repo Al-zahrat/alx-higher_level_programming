35dce4483a87579b388471f88c44b6a5059043e9
ezzahra <sindyzahra26@gmail.com>
2024-06-20 04:58:39 +0000

    javascript

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -8,10 +8,8 @@ class Rectangle {
   }
 
   print () {
-    for (let i = 0; i < this.width; i++) {
-      for (let j = 0; j < this.height; j++) {
-        console.log('X');
-      }
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
     }
   }
 }
