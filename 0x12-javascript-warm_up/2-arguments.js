1bedd8b901e14c707ca2d86c0462a8a6fc1759db
ezzahra <sindyzahra26@gmail.com>
2024-06-21 01:59:52 +0000

    javascript

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,4 +1,9 @@
 #!/usr/bin/node
-import { argv } from 'node:process';
-argv.forEach((val, index) => {
-	if (index == 
+const args = process.argv.slice(2);
+if (args.length === 0) {
+  console.log('No argument');
+} else if (args.length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
