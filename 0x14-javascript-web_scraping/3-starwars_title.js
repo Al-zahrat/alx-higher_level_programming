836ebd2c141f93e7a25e4afdfb6c1a1987813ef6
ezzahra <sindyzahra26@gmail.com>
2024-07-30 23:30:40 +0000

    web scraping

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const request = require('request');
+const num = process.argv[2];
+request('https://swapi-api.alx-tools.com/api/films/' + num, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
